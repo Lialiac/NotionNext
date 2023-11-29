8fe8f1acc9d599c35d94301d456d0b75223c0f94
Lialiac <152414746+Lialiac@users.noreply.github.com>
2023-11-29 20:32:15 +0100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Avro Avian', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Fashion Brand', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://avro-avian.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.avro-avian.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Avro Avian, Fashion Brand', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
